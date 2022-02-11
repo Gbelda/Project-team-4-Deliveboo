54cbcdc43f3cf553b5b2367aedd356a86377ced4
Paolo <pascuccip9@gmail.com>
2022-02-11 11:43:36 +0100

    Add Order and plate Controller admin side

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,27 +1,23 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
-                </div>
-
-                <div class="image">
-                    <img src="{{ asset('storage/'. $user->image) }}" alt="">
-                    <h1>add description</h1>
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                        <H1>Ben Arrivato {{ $user->name }}</H1>
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
