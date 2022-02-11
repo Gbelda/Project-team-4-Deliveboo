5054eef859d359fe544b7315037d42252a02b56f
Marcoseven <marco.caliciotti3@gmail.com>
2022-02-11 11:39:34 +0100

    Add models plate and order, and more.

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,27 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                    {{ __('You are logged in!') }}
-                </div>
+    @include('admin.dashboard')
 
-                <div class="image">
-                    <img src="{{ asset('storage/'. $user->image) }}" alt="">
-                    <h1>add description</h1>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
