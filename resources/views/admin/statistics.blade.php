19bc5b2725b0b61de08073d02d21b82a078d2ac2
Marcoseven <marco.caliciotti3@gmail.com>
2022-02-25 13:03:31 +0100

    Add statistical changes.

--- resources/views/admin/statistics.blade.php
+++ resources/views/admin/statistics.blade.php
@@ -3,10 +3,16 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.min.js" defer></script>
     <script src="{{ asset('js/chart.js') }}" defer></script>
 @endsection
+
 @section('content')
     <div class="container">
         <h1 class="text-center">Statistiche</h1>
-        <canvas id="myChartBar" class="mb-5"></canvas>
-        <canvas id="myChartLine"></canvas>
+        <h2 class="text-center">Statistiche ordini ricevuti</h2>
+        <canvas id="chartBarOrders" class="mb-5"></canvas>
+        <canvas id="chartLineOrders" class="mb-5"></canvas>
+
+        <h2 class="text-center">Statistiche ricavato</h2>
+        <canvas id="chartBarSales" class="mb-5"></canvas>
+        <canvas id="chartLineSales"></canvas>
     </div>
 @endsection
