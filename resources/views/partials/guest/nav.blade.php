c36bc195ed4f2702c02d345055b3166a199921c1
gbelda <gbelda56@gmail.com>
2022-02-28 10:53:40 +0100

    Change admin nav

--- resources/views/partials/guest/nav.blade.php
+++ resources/views/partials/guest/nav.blade.php
@@ -1,16 +1,17 @@
 <nav id="nav_guest" class="container-fluid">
     <div class="nav_guest container d-flex justify-content-between align-items-center">
         <div class="col-2">
-            <a href="{{ url('/') }}"><img src="{{ asset('./img/jumbo/logo.png') }}" alt="immagine logo Deliveboo"></a>
+            <a href="{{ url('/') }}"><img src="{{ asset('./img/jumbo/logo.png') }}"
+                    alt="immagine logo Deliveboo"></a>
         </div>
         <div class="hamburgen">
-            <div class="dropdown">
-                <button class="button_navbar btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    Sei un ristorante?
-                </button>
-                <ul class="dropdown-menu tendina" aria-labelledby="dropdownMenuButton1">
-                    @guest
+            @guest
+                <div class="dropdown">
+                    <button class="button_navbar btn btn-link dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+                        Sei un ristorante?
+                    </button>
+                    <ul class="dropdown-menu tendina" aria-labelledby="dropdownMenuButton1">
                         <li class="nav-item nav_link">
                             <a class="nav-link" href="{{ route('login') }}">{{ __('Accedi') }}</a>
                         </li>
@@ -19,28 +20,27 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Registra') }}</a>
                             </li>
                         @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link text-center brand-color"
-                                href="{{ route('admin.index') }}" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                    </ul>
+                @else
+                    <div class="dropdown">
+                        <a id="navbarDropdown" class="nav-link text-center brand-color" href="{{ route('admin.index') }}"
+                            aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                                            document.getElementById('logout-form').submit();">
+                                {{ __('Esci') }}
                             </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                                document.getElementById('logout-form').submit();">
-                                    {{ __('Esci') }}
-                                </a>
-
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                    class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-
-                </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </div>
+                @endguest
             </div>
             {{-- <i class="fa-solid fa-bars"></i> --}}
         </div>
